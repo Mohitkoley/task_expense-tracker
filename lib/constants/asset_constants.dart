01cbc55710c9640f78dce27adde1365d7d0732be
Mohit koley <mohitkoley5@gmail.com>
2025-06-23 12:46:47 +0530

    added fgen

---
+++ lib/constants/asset_constants.dart
@@ -0,0 +1,10 @@
+/// Generate by [asset_generator](https://github.com/fluttercandies/flutter_asset_generator) library.
+/// PLEASE DO NOT EDIT MANUALLY.
+// ignore_for_file: constant_identifier_names
+class AssetConstants {
+  const AssetConstants._();
+
+  /// ![preview](file:///Users/mohitkoley/flutter_projects/tasks/task_expense-tracker/assets/images/png/no_task.png)
+  static const String ASSETS_IMAGES_PNG_NO_TASK_PNG =
+      'assets/images/png/no_task.png';
+}
